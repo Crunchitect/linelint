5015a781a5ccde5cc90ed8968faebc090be927c3
Crunchitect <prem.sy1952@gmail.com>
2024-02-27 21:06:35 +0700

    real

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="center"><img src="@/assets/image.gif" alt=""></div>
+</template>
+
+<style scoped>
+    .center {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,7 @@
 <template>
-  <p>Real</p>
+  <Header></Header>
 </template>
+
+<script setup lang="ts">
+  import Header from '@/components/Header.vue';
+</script>
